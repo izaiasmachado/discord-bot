8b77f42c07e309ed455ef425094c94c111731a5d
izaiasmachado <izaiasmachado@icloud.com>
2019-09-07 13:24:01 -0300

    feat: added event message format

--- src/events/message.js
+++ src/events/message.js
@@ -4,5 +4,26 @@ module.exports = (bot, message) => {
     if (message.content[0] !== '!') {
         return false
     }
-    
+
+}
+
+function format(msg) {
+    let cmd = ''
+    let args = ''
+    let cond = false
+
+    for(let i = 1; i < msg.length; i++) {
+        if (msg[i] === ' ') {
+            cond = true
+        } else {
+            if (cond) {
+                args += msg[i]
+            } else {
+                cmd += msg[i]
+            }
+        }
+    }
+
+    const response = [cmd, args]
+    return response
 }
