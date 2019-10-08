bd4b0560a72a013436c3dac470135b34065d96a9
Izaias Machado <izaiasmachado@icloud.com>
2019-10-08 20:58:57 -0300

    fix: guild is not deleted from DB anymore

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -4,14 +4,18 @@ module.exports = async (bot, member) => {
     const guild = member
     const serverId = guild.id
 
-    const server = await serverList.create({
+    const serverExists = await serverList.findOne({ serverId })
+    
+    console.log(`joined ${guild.name}!`)
+    
+    if (serverExists) {
+        return true
+    } 
+
+    await serverList.create({
         serverId,
         key: '!'
     })
-
-    if (server) {
-        console.log(`joined ${guild.name}!`)
-    }
 
     let name = 'perry-dashboard';
     const dashboardExists = guild.channels.find(ch => ch.name == name)
@@ -26,7 +30,7 @@ module.exports = async (bot, member) => {
             .then(() => {
                 const dashboard = guild.channels.find(ch => ch.name == name)
 
-                messageModule({
+                return messageModule({
                     user: guild.owner,
                     guild: guild.id,
                     content: `Hi, thanks for inviting me to **{server}**! I hope to be usefull, just make sure to look at ${dashboard} and get to know me better :stuck_out_tongue_winking_eye:`,
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,10 +1,5 @@
-const serverList = require('../models/Guild')
-
 module.exports = async (bot, member) => {
     const guild = member
-    const serverId = guild.id
     
-    await serverList.deleteOne({ serverId })
-
     return console.log(`left ${guild.name}`)
 }
